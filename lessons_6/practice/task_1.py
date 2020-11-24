d6dfc450ead421fff00d9941e32770acf34dcb3d
Rustam <rystamrv@gmail.com>
2020-11-24 19:31:12 +0200

    update errors

--- lessons_6/practice/task_1.py
+++ lessons_6/practice/task_1.py
@@ -15,8 +15,11 @@ class MyList:
             return x
         raise StopIteration
 
+    def __add__(self, other):
+        return MyList(self.my_list + other.my_list)
+
     def add(self, x):
-        self.my_list.append(x)
+        self.my_list[len(self.my_list):] = [x]
 
     def insert(self, ind, x):
         if len(self.my_list) <= ind:
@@ -26,21 +29,20 @@ class MyList:
 
     def remove(self, x):
         if x in self.my_list:
-            self.my_list.remove(x)
+            self.my_list = [i for i in self.my_list if x!=i] 
         else:
             raise ValueError(f'{x} not in list')
 
     def clear(self):
-        self.my_list.clear()
+        self.my_list = []
     
     def pop(self, x=None):
-        if x == None:
-           return self.my_list.pop()
-        return self.my_list.pop(x)
-
-    def add_new_list(self, array):
-        return MyList(self.my_list+array)
-
+        if x == None: 
+            last = self.my_list[len(self.my_list)-1:][0] 
+            self.remove(last)
+            return last
+        self.remove(x)
+        return x 
 
 
 mylist = MyList()
@@ -49,20 +51,20 @@ mylist.add(1)
 mylist.add(2)
 mylist.add(34)
 print(mylist.my_list)
-mylist.insert(3, 6)
-print(mylist.my_list)
-mylist.insert(4, 6)
-print(mylist.my_list)
-mylist.insert(0, 21)
-print(mylist.my_list)
+# mylist.remove(34)
+# print(mylist.my_list) 
+# mylist.insert(0, 21)
+# print(mylist.my_list)
   
-print(mylist.pop())
+print(mylist.pop(5))
 print(mylist.my_list)
 
-print(mylist.pop(0))
-print(mylist.my_list)
-for i in mylist:
-    print(i)
+# print(mylist.pop(0))
+# print(mylist.my_list)
+# for i in mylist:
+#     print(i)
 
-new_list = mylist.add_new_list([122,4,4,5,6,])
+array = MyList([122,4,4,5,6,])
+
+new_list = mylist + array
 print(new_list.my_list)
